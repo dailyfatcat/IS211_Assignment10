e2a4ab893d72d54a16b903eab2ea684f237c05f6
dailyfatcat <50534920+dailyfatcat@users.noreply.github.com>
2021-04-17 18:03:17 -0400

    Assignment 10

--- music.sql
+++ music.sql
@@ -1,1 +1,28 @@
 
+/*******************************************************************************
+   Assignment Part 1 Music Database
+
+Modeling music artists, the albums they create and the songs that appear on those albums
+
+A set of relational tables that will sufficiently model what is described above.
+Submit a file named music.sql, with SQL CREATE queries that will create the tables with the proper schema of your model.
+********************************************************************************/
+
+CREATE TABLE artist
+( id INTEGER PRIMARY KEY ASC,
+    name CHAR(100),
+);
+
+CREATE TABLE album
+( id INTEGER PRIMARY KEY ASC,
+    name CHAR(100),
+    artist CHAR(100)
+);
+
+CREATE TABLE song
+( id INTEGER PRIMARY KEY ASC,
+    name CHAR(100),
+    track INTEGER,
+    length INTEGER,
+    album CHAR(100)
+);
