2861b8f5c76ef84012b522c3e68a3fb6fbe81302
dailyfatcat <50534920+dailyfatcat@users.noreply.github.com>
2021-04-17 21:44:53 -0400

    Assignment 10

--- query_pets.py
+++ query_pets.py
@@ -14,29 +14,27 @@ def main(args):
         while choice != '-1':
             #If the user id does not exist print an error message
             try:
-
-               # cur.execute("SELECT * from person Join person_pet where id== person_id ")
                 cur.execute("SELECT person_pet.pet_id, person_pet.person_id, person.first_name, person.last_name,person.age, pet.age, pet.name, pet.breed, pet.dead "
-                            "FROM person_pet "
-                            "LEFT JOIN person on person_pet.person_id = person.id "
-                            "LEFT JOIN pet on person_pet.pet_id = pet.id "
-                            "where person.id ="+choice)
+                                "FROM person_pet "
+                                "LEFT JOIN person on person_pet.person_id = person.id "
+                                "LEFT JOIN pet on person_pet.pet_id = pet.id "
+                                "where person.id ="+choice)
 
                 row = cur.fetchall()
+                print(f"{row[0][2]} {row[0][3]}, is {row[0][4]} years old")
                 for r in row:
-                    if row[8] == 1:
-                        #If the animal is dead print accordingly
-                        print(f"{row[3]} {row[4]},{row[5]}years old")
-                        print(f"{row[3]} {row[4]} owned {row[6]}, a {row[7]}, that was {row[5]} years old")
+                    if r[8] == 1:
+                            #If the animal is dead print accordingly
+                        print(f"{r[2]} {r[3]} owned {r[6]}, a {r[7]}, that was {r[5]} years old")
                     else:
-                        #Else print as if the animal is alive
-                        print(f"{row[3]} {row[4]},{row[5]}years old")
-                        print(f"{row[3]} {row[4]} owns {row[6]}, a {row[7]}, that is {row[5]} years old")
-            except:
+                            #Else print as if the animal is alive
+                        print(f"{r[2]} {r[3]} owns {r[6]}, a {r[7]}, that is {r[5]} years old")
+            except ValueError:
                 print('Error There is no Person with that ID')
 
             #Read in the users choice
             print("Enter the person ID")
+
             choice = input()
 
 
